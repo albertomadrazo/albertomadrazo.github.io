6cf4373523a188514e83b4f24b9779a7f7adaadc
otreblatercero <otreblatercero@gmail.com>
2016-01-20 18:51:38 -0600

    Add an email form.

--- js/scripts.js
+++ js/scripts.js
@@ -90,5 +90,20 @@ $(document).ready(function(){
 		}
 	});
 
+
+		function getParameterByName(name) {
+    	var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+    	return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+	}
+
+		function mailSent(){
+			var sent = getParameterByName('email_sent');
+			
+			if(sent){
+				alert("Gracias por ponerte en contacto, tendras respuesta en breve.");
+			}
+		}
+
+	mailSent();
 });
 
